907ca06632f6ad1271019f38a0c880cbac291873
Ekaluk52003 <ekaluk.p@gmail.com>
2024-09-09 22:34:03 +0700

    dgc

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-   if [ "$DATABASE" = "postgres" ]
-   then
-       echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-       while ! nc -z $SQL_HOST $SQL_PORT; do
-         sleep 1
-       done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 1
+    done
 
-       echo "PostgreSQL started"
-   fi
+    echo "PostgreSQL started"
+fi
 
-   # Collect static files
-   echo "Collecting static files..."
-   python manage.py collectstatic --noinput
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
-   # Apply database migrations
-   echo "Applying database migrations..."
-   python manage.py migrate
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
 
-   # Start Gunicorn
-   echo "Starting Gunicorn..."
-   exec gunicorn tour.wsgi:application --bind 0.0.0.0:8000
+# Start Gunicorn
+echo "Starting Gunicorn..."
+exec gunicorn tour.wsgi:application --bind 0.0.0.0:8000

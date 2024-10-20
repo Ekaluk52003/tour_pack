310767e9091b06332cbcf115bac2a4129444f2f1
Ekaluk52003 <ekaluk.p@gmail.com>
2024-10-20 15:24:16 +0700

    update URL

--- tours/urls.py
+++ tours/urls.py
@@ -3,18 +3,18 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    # put save-tour-package as it may match to tour_package_detail or other route
     path('save-tour-package/', views.save_tour_package, name='save_tour_package'),
-    path('save-tour-package/<str:package_reference>/', views.save_tour_package, name='update_tour_package'),
+    path('save-tour-package/<int:package_reference>/', views.save_tour_package, name='update_tour_package'),
     path('old-list', views.tour_package_list, name='tour_package_list'),
     path('create/', views.tour_package_quote, name='tour_package_quote'),
-    path('<str:package_reference>/', views.tour_package_detail, name='tour_package_detail'),
-    path('<str:package_reference>/edit/', views.tour_package_edit, name='tour_package_edit'),
+    path('<int:package_reference>/', views.tour_package_detail, name='tour_package_detail'),
+    path('<int:package_reference>/edit/', views.tour_package_edit, name='tour_package_edit'),
     path('get-city-services/<int:city_id>/', views.get_city_services, name='get_city_services'),
     path('tour-package/<int:pk>/pdf/', views.tour_package_pdf, name='tour_package_pdf'),
     path('', views.tour_packages, name='tour_packages'),
     path('get-predefined-tour-quote/<int:quote_id>/', views.get_predefined_tour_quote, name='get_predefined_tour_quote'),
     path('tour-package/<int:pk>/send-email/', views.send_tour_package_email, name='send_tour_package_email'),
-
 
 ]
 
